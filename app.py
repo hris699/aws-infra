a9564fb695cf001c93dbcc3afa32a3300639b01d
hrishabh gohiya <hrishabh.gohiya@yash.com>
2025-05-09 14:37:41 +0530

    updated

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import aws_cdk as cdk
 from my_cdk_app.my_cdk_app_stack import MyCdkAppStack
 
 app = cdk.App()
-MyCdkAppStack(app, "MyCdkAppStack", env=env)
+MyCdkAppStack(app, "MyCdkAppStack")
 
 app.synth()
